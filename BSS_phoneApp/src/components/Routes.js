af1a212c67afc2a1b40a918b3b41b176941009cf
Nsinha23 <Nsinha23@users.noreply.github.com>
2021-01-12 14:36:23 -0800

    SignupPage
    Signup page + added the page in routes

--- BSS_phoneApp/src/components/Routes.js
+++ BSS_phoneApp/src/components/Routes.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Router, Scene } from 'react-native-router-flux'
 import Home from '../views/Home.js'
 import About from '../views/About.js'
+import Signup from '../views/Signup.js';
 
 const Routes = () => (
     <Router>
         <Scene key="root">
             <Scene key="home" component={Home} title="Home" initial={true} />
             <Scene key="about" component={About} title="About" />
+            <Scene key="signup" component={Signup} title="Signup"  />
         </Scene>
     </Router>
 )
---
+++ BSS_phoneApp/src/views/Signup.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { StyleSheet, Text, View,TextInput,TouchableOpacity,StatusBar } from 'react-native';
+import { withNavigation } from 'react-navigation';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+import { Actions } from 'react-native-router-flux'
+
+const Signup = () => {
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.signupTextCont}>
+        <TextInput style={styles.inputBox}
+          placeholder="Email"
+          placeholderTextColor='#212121'
+        />
+        <TextInput style={styles.inputBox}
+          placeholder="Password"
+          secureTextEntry={true}
+          placeholderTextColor='#212121'
+        />
+        <TextInput style={styles.inputBox}
+          placeholder="Confirm Password"
+          secureTextEntry={true}
+          placeholderTextColor='#212121'
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}> Create Account </Text>
+        </TouchableOpacity>
+
+        <StatusBar
+          backgroundColor="#005661"
+          barStyle="light-content"
+        />
+        <StatusBar style="auto" />
+
+      </View>
+    </View>
+
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#00838e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputBox: {
+    width: 200,
+    height: 30,
+    backgroundColor: '#ededed',
+    borderRadius: 40,
+    paddingHorizontal: 16,
+    fontSize: 14,
+    color: '#212121',
+    marginVertical: 10
+
+  },
+  button: {
+    backgroundColor: '#ededed',
+    borderRadius: 60,
+    width: 200,
+    marginVertical: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 4
+
+},
+buttonText: {
+    fontSize: 14,
+    fontWeight: '500',
+    color: '#212121',
+    textAlign: 'center'
+},
+signupTextCont:{
+  fontSize: 16,
+  fontWeight: '500',
+  color: '#ffffff',
+  textAlign: 'center' 
+},
+signupText: {
+  color: '#ffffff',
+  fontSize:18,
+  textAlign: 'center',
+  marginVertical: 10,
+},
+signupbutton:{
+  color:'#ffffff' , 
+  fontSize: 18, 
+  fontWeight:'500',
+  textAlign: 'center',
+  marginVertical: 10,
+},
+
+
+});
+
+export default Signup;
