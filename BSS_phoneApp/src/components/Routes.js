a20bf5eb81340d5508e79a15014be9ddcf02d383
Nsinha23 <Nsinha23@users.noreply.github.com>
2021-01-12 18:43:46 -0800

    Added a signup
    Added signup page

--- BSS_phoneApp/src/components/Routes.js
+++ BSS_phoneApp/src/components/Routes.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Router, Scene } from 'react-native-router-flux'
 import Home from '../views/Home.js'
 import About from '../views/About.js'
+import Signuppage from '../views/Signuppage.js';
 
 const Routes = () => (
     <Router>
         <Scene key="root">
             <Scene key="home" component={Home} title="Home" initial={true} />
             <Scene key="about" component={About} title="About" />
+            <Scene key="signuppage" component={Signuppage} title="Signuppage"/>
         </Scene>
     </Router>
 )
