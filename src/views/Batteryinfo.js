723888ee28fc703140cc391b8a50cdc1bf88141c
Nsinha23 <Nsinha23@users.noreply.github.com>
2021-04-19 12:01:44 -0700

    Battery Status Update
    - Update Charge, Temperature, Helath
    - Click on Img + change UBP accordingly

--- src/views/Batteryinfo.js
+++ src/views/Batteryinfo.js
@@ -1,103 +1,133 @@
-import React, { useState } from 'react'
-import {
-    TouchableOpacity,
-    Text,
-    TextInput,
-    ScrollView,
-    StatusBar,
-    Image,
-    SafeAreaView,
-    StyleSheet,
-    Button,
-    Platform,
-    RefreshControl
-} from 'react-native'
+import { Component } from "react";
+import React from 'react'
+import { TouchableOpacity, Text, View, StatusBar, Image, StyleSheet, TouchableHighlight, Button } from 'react-native'
+
+const images = {
+    batteryimage: {
+        green: require('../assets/greenb.png'), // >= 75
+        yellow: require('../assets/yellowb.png'), // >= 50
+        red: require('../assets/redb.png'), // > = 25
+        dead: require('../assets/deadcell.png')
+    }
+
+};
 
 
-const wait = (timeout) => {
-    return new Promise(resolve => setTimeout(resolve, timeout));
-}
+export default class just extends Component {
+// intial states
+    state = {
+        Charge: 10,
+        Health: 'Good',
+        img: images.batteryimage.green,
+        NumbHolder: 1,
+        UBP_check:'Click battery',
+        tempchecker:0,
+    }
+    constructor() {
+        super()
 
+    }
+    battery1 = () =>{
+        this.setState({UBP_check: 1})
+    }
+    battery2 = () =>{
+        this.setState({UBP_check: 2})
+    }
+    battery3 = () =>{
+        this.setState({UBP_check: 3})
+    }
 
-
-const Batteryinfo = () => {
-
-    const [refreshing, setRefreshing] = React.useState(false);
-    const [UBP, setUBP] = useState({
-        temperature: '',
-        voltage: '',
-        charge: ''
-    });
-
-    const onRefresh = React.useCallback(() => {
-        setRefreshing(true);
-        //imagepath = "whats up"
-
-        wait(2000).then(() => setRefreshing(false));
-    });
-
-    const images = {
-        batteryimage: {
-            green: require('../assets/greenb.png'),
-            yellow: require('../assets/yellowb.png'),
-            red: require('../assets/redb.png'),
-            dead: require('../assets/deadcell.png')
+    // generates random number between 1-100
+    RandomNumGenerator = () => {
+        var RandNumb = Math.floor(Math.random() * 100) + 1; // status of charge
+        this.setState({ NumbHolder: RandNumb })      
+        if (RandNumb >= 66 ) {
+            this.setState({ img: images.batteryimage.green })   
         }
+        else if (RandNumb >= 33) {
+            this.setState({ img: images.batteryimage.yellow })   
+        }
+        else {
+            this.setState({ img: images.batteryimage.dead })
+        }
+        this.TemperatureSetter()
+    }
 
-    };
+    // Based on Temperature -> Health gets changed
+    TemperatureSetter = () => {
+        var temp =  Math.floor(Math.random() * 100) + 1;    // temperature
+        this.setState({tempchecker: temp})
+        if (temp >= 70){
+            this.setState({ Health: 'Poor' })    // if temp >= 70 : Health = Poor
+        }
+        else if (temp >= 30){
+            this.setState({ Health: 'Average' })  // if temp >= 30 : Health = Average
+        }
+        else{
+            this.setState({ Health: 'Good' })     // otherwise Health: Good
+        }        
+    }
 
+// 
 
+    render() {
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <ScrollView contentContainerStyle={styles.scrollView}
-                refreshControl={
-                    <RefreshControl
-                        refreshing={refreshing}
-                        onRefresh={onRefresh}
-                    ></RefreshControl>
-                }
-            >
-                 <Button title="Click Here To Refresh" />
+        return (
 
-                <Image style={styles.batteryImage}
-                    source={images.batteryimage.green}
+            <View style={styles.container}>
+
+                <Button
+                    title="Update Status"
+                    onPress={this.RandomNumGenerator}  // update button
                 />
-                <Image style={styles.batteryImage}
-                    source={images.batteryimage.yellow}
-                />
-               
-                <Image style={styles.batteryImage}
-                    source={require('../assets/redb.png')}
-                />
-                <Image style={styles.batteryImage}
-                    source={require('../assets/deadcell.png')}
-                />
+                 <TouchableOpacity onPress={this.battery1} >
+                    <Image style={styles.batteryImage}      // changeable image
+                    source={this.state.img}/> 
+                 </TouchableOpacity>
+                
+                <TouchableOpacity onPress={this.battery2}>
+                    <Image style={styles.batteryImage}      // second battery img
+                    source={require('../assets/redb.png')} />                     
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={this.battery3} >
+                    <Image style={styles.batteryImage}      // third battery img
+                    source={require('../assets/deadcell.png')} />
+                </TouchableOpacity>
+
                 <StatusBar
                     backgroundColor="#005661"
-                    barStyle="light-content"
+                    barStyle="light-content"                // status bar
                 />
                 <StatusBar style="auto" />
+                
+                    
+                <TouchableHighlight style={styles.button}>   
+                    <Text style={styles.statusText}> UBP: {this.state.UBP_check} </Text>
+                </TouchableHighlight>
 
-                <TouchableOpacity style={styles.button}>
-                    <TextInput style={styles.statusText}> UBP: Unknown </TextInput>
-                </TouchableOpacity>
+                <TouchableHighlight style={styles.button}>
+                    <Text style={styles.statusText}> Charge: {this.state.NumbHolder} % </Text>
+                </TouchableHighlight>
 
-                <TouchableOpacity style={styles.button}>
-                    <Text style={styles.statusText}> Charge: Unknown </Text>
-                </TouchableOpacity>
+                <TouchableHighlight style={styles.button}>
+                    <Text style={styles.statusText}> Temperature: {this.state.tempchecker}{'\u00b0'}C </Text>
+                </TouchableHighlight>
 
-                <TouchableOpacity style={styles.button}>
-                    <Text style={styles.statusText}> Temperature: Unknown  </Text>
-                </TouchableOpacity>
+                <TouchableHighlight style={styles.button}>
+                    <Text style={styles.statusText}> Health: {this.state.Health}  </Text>
+                </TouchableHighlight> 
 
-                <TouchableOpacity style={styles.button}>
-                    <Text style={styles.statusText}> Health: Unknown </Text>
-                </TouchableOpacity>
-            </ScrollView>
-        </SafeAreaView>
 
-    )
+
+
+
+            </View>
+        );
+
+    }
+
+
 }
 const styles = StyleSheet.create({
     container: {
@@ -108,9 +138,9 @@ const styles = StyleSheet.create({
 
     button: {
         width: 290,
-        height: 80,
+        height: 120,
         left: 160,
-        bottom: 320,
+        bottom: 520,
         backgroundColor: '#ededed',
         borderRadius: 40,
         paddingHorizontal: 16,
@@ -120,19 +150,20 @@ const styles = StyleSheet.create({
         marginVertical: 10
 
     },
+
     statusText: {
         color: 'black',
         textAlign: 'left',
-        fontSize: 14,
+        fontSize: 17,
         fontWeight: 'bold',
         padding: 30
 
     },
     batteryImage: {
         width: 90,
-        top: 90,
+        top: 40,
         left: 35,
-        height: 110,
+        height: 180,
     },
     scrollView: {
         flex: 1,
@@ -141,4 +172,27 @@ const styles = StyleSheet.create({
     }
 
 });
-export default Batteryinfo
+/** */
+/**
+ *                 <Button
+                    title="Update state"
+                    onPress={this.updateUsername} />
+                <Text>{this.state.username}</Text>
+ *
+ *
+ *                  updateUsername = () => {
+        if (this.state.updateuser) {
+            this.setState({ username: 'Hannsel' })
+            this.setState({ updateuser: false })
+            this.setState({ blah: images.batteryimage.yellow })
+        }
+        else if (!this.state.updateuser) {
+            this.setState({ username: 'KIP' })
+            this.setState({ updateuser: true })
+            this.setState({ blah: images.batteryimage.red })
+        }
+    }
+
+ * <button onClick={this.updateUsername}>Refresh name</button>
+                <> Username: {this.state.username}</>
+ */
