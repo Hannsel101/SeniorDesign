ca7a2f74aee58d527dd31e8da2d2a41ad2be9829
Nsinha23 <Nsinha23@users.noreply.github.com>
2021-05-09 20:31:25 -0700

    Battery Info check for Bluetooth
    Battery info checks if bluetooth is connected or not

--- src/views/Home.js
+++ src/views/Home.js
@@ -6,9 +6,6 @@ import Video from "react-native-video";
 
 const { width, height } = Dimensions.get("window");
 const Home = () => {
-    const goToBluetooth = () => {
-        Actions.bluetooth()
-    }
     const goToLocation = () => {
         Actions.location()
     }
@@ -49,9 +46,6 @@ const Home = () => {
             source ={require('../assets/white_logo.png')}
              />
              
-             <TouchableOpacity style={styles.button} onPress={goToBluetooth}>
-                <Text style={styles.buttonText}> Bluetooth </Text>
-            </TouchableOpacity>
             <TouchableOpacity style={styles.button} onPress={goToLogin}>
                 <Text style={styles.buttonText}> Login </Text>
             </TouchableOpacity>
